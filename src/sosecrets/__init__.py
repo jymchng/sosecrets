37e023f3374289a78ac49d1f8feb5f53cc8f13f1
Soon Siang <jymchng@hotmail.com>
2023-05-28 02:21:52 +0800

    bump version 0.1.3 -> 0.1.4

--- src/sosecrets/__init__.py
+++ src/sosecrets/__init__.py
@@ -1,4 +1,4 @@
 from sosecrets.secrets import Secret
 from sosecrets.secretdicts import MutableSecretMapping, ImmutableSecretMapping
 
-__version__ = version = "0.1.3"
+__version__ = version = "0.1.4"
