a6ca6c4309046b64087de03ec442ecf07f289f8d
Soon Siang <jymchng@hotmail.com>
2023-05-30 11:36:38 +0800

    testing rebuild dev

--- src/sosecrets/secretdicts.py
+++ src/sosecrets/secretdicts.py
@@ -1,8 +1,8 @@
 from sosecrets_core.secrets import Secret
-from typing import Dict, Any, Optional, Callable, Generic, T, Tuple
+from typing import Dict, Any, Optional, Callable, Tuple, Mapping
 
 
-class ImmutableSecretMapping(dict, Dict[Any, Secret]):
+class ImmutableSecretMapping(dict, Mapping[Any, Secret]):
     """
     A dictionary-like object that stores secret values and prevents changes to the dictionary.
     """
@@ -35,7 +35,7 @@ class ImmutableSecretMapping(dict, Dict[Any, Secret]):
             value (Any): The value to set.
 
         Raises:
-            Exception: When attempting to set a new key-value pair.
+            TypeError: When attempting to set a new key-value pair.
         """
         raise TypeError(
             "ImmutableSecretMapping object does not support item assignment.")
@@ -58,7 +58,6 @@ class ImmutableSecretMapping(dict, Dict[Any, Secret]):
 
         Raises:
             ValueError: If the result of the function is empty.
-            TypeError: If any of the values in the result are not of type Secret.
         """
         mapping = func(*func_args, **func_kwargs)
         if not mapping:
@@ -80,24 +79,24 @@ class ImmutableSecretMapping(dict, Dict[Any, Secret]):
 
         Returns:
             Any: The exposed value of the key, or the default value if the key is not found."""
-        value = super().get(key, default)
+        value: Secret = super().get(key, default)
         if value == default:
             return value
         return value.expose_secret()
 
     def expose_dict(self) -> Dict[Any, Any]:
         """
-        Return a new dictionary that exposes non-sensitive information.
+        Return a new dictionary that exposes exposed information.
 
-        This method returns a new dictionary with the same keys as the original dictionary, but with values that have been converted to non-sensitive information using their `expose_secret()` method. The `expose_secret()` method is expected to return a non-sensitive version of the value, so that the dictionary can be safely exported or displayed without exposing sensitive information.
+        This method returns a new dictionary with the same keys as the original dictionary, but with values that have been converted to exposed information using their `expose_secret()` method. The `expose_secret()` method is expected to return a exposed version of the value, so that the dictionary can be safely exported or displayed without exposing sensitive information.
 
         Returns:
-            Dict[Any, Any]: A new dictionary with non-sensitive values.
+            Dict[Any, Any]: A new dictionary with exposed values.
         """
         return {k: v.expose_secret() for k, v in super().items()}
 
 
-class MutableSecretMapping(dict):
+class MutableSecretMapping(dict, Mapping[Any, Secret]):
     """
     A dictionary-like object that stores secret values and allows changes to the dictionary.
     """
@@ -107,12 +106,9 @@ class MutableSecretMapping(dict):
 
         Args:
             mapping (Dict[Any, Any]): A dictionary with `Any` keys and `Any` values.
-
-        Raises:
-            ValueError: If the mapping argument is empty.
         """
         if not mapping:
-            raise ValueError("Mapping argument is empty.")
+            return super().__new__(cls, mapping)
         new_mapping = {}
         for k, v in mapping.items():
             if not isinstance(v, Secret):
@@ -123,26 +119,26 @@ class MutableSecretMapping(dict):
 
     def __setitem__(self, key: Any, value: Any) -> None:
         """
-        Set a new key-value pair in the MutableSecretMapping object.
+        Set a new key-value pair in the `MutableSecretMapping` object.
 
         Parameters:
             key (Any): The key to set.
             value (Any): The value to set.
 
         Raises:
-            ValueError: If the value is not of type Secret.
+            ValueError: If the value is not of type `Secret`.
         """
         if not isinstance(value, Secret):
-            raise ValueError("Value must be of type Secret.")
+            raise ValueError("Value must be of type `Secret`.")
         super().__setitem__(key, value)
 
     @classmethod
     def from_func(
             cls,
-            func: Callable[[Any], Dict[Any, Any]],
-            *func_args: Tuple[Any],
+            func: Callable[..., Dict[Any, Any]],
+            *func_args: Tuple[Any, ...],
             **func_kwargs: Dict[Any, Any]) -> 'MutableSecretMapping':
-        """Create a new MutableSecretMapping object from the result of a function.
+        """Create a new `MutableSecretMapping` object from the result of a function.
 
         Args:
             func (callable): A function that returns a dictionary with `Any` keys and secret values.
@@ -150,11 +146,10 @@ class MutableSecretMapping(dict):
             **func_kwargs (Any): Keyword arguments to pass to the function.
 
         Returns:
-            MutableSecretMapping: A new MutableSecretMapping object.
+            MutableSecretMapping: A new `MutableSecretMapping` object.
 
         Raises:
             ValueError: If the result of the function is empty.
-            TypeError: If any of the values in the result are not of type Secret.
         """
         mapping = func(*func_args, **func_kwargs)
         if not mapping:
@@ -169,7 +164,7 @@ class MutableSecretMapping(dict):
 
     def get_exposed(self, key: Any, default: Optional[Any] = None) -> Any:
         """
-        Get the exposed value of a key in the MutableSecretMapping object.
+        Get the exposed value of a key in the `MutableSecretMapping` object.
 
         Args:
             key (Any): The key to get.
@@ -178,18 +173,19 @@ class MutableSecretMapping(dict):
         Returns:
             Any: The exposed value of the key, or the default value if the key is not found.
         """
-        value = super().get(key, default)
+        value: Secret = super().get(key, default)
         if value == default:
             return value
         return value.expose_secret()
 
     def expose_dict(self) -> Dict[Any, Any]:
         """
-        Return a new dictionary that exposes non-sensitive information.
+        Return a new dictionary that exposes information.
 
-        This method returns a new dictionary with the same keys as the original dictionary, but with values that have been converted to non-sensitive information using their `expose_secret()` method. The `expose_secret()` method is expected to return a non-sensitive version of the value, so that the dictionary can be safely exported or displayed without exposing sensitive information.
+        This method returns a new dictionary with the same keys as the original dictionary, 
+        but with values that have been converted to exposed information using their `expose_secret()` method.
 
         Returns:
-            Dict[Any, Any]: A new dictionary with non-sensitive values.
+            Dict[Any, Any]: A new dictionary with exposed values.
         """
         return {k: v.expose_secret() for k, v in super().items()}
